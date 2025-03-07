c70cfa80d28ee97c4cf665fe04af24d43c22edd1
Daniel Granados <20200008@ricaldone.edu.sv>
2025-03-07 10:40:40 -0600

    delete and update

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -6,11 +6,34 @@ import productsModel from "../models/Products.js"
 productsController.getProducts = async (req, res) => {
     const products = await productsModel.find()
     res.json(products)
-}
+};
 
 // INSERT
 productsController.createProducts = async (req, res) => {
     const { name, description, price, stock } = req.body;
     const newProduct = new productsModel({ name, description, price, stock });
     await newProduct.save()
-}
+    res.json({ message: "Product saved"})
+};
+
+// DELETE
+productsController.deleteProducts = async (req, res) => {
+    await productsModel.findOneAndDelete(req.params.id)
+    res.json({ message: "Product deleted"})
+};
+
+// UPDATE
+productsController.updateProducts = async (req, res) => {
+    const { name, description, price, stock } = req.body;
+    await productsModel.findByIdAndUpdate(req.params.id, {
+        name,
+        description,
+        price,
+        stock
+    }, {new: true}
+    );
+
+    res.json({ message: "Product updated"})
+};
+
+export default productsController;
