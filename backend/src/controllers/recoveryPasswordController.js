f0d9bc8f2f31e8cb01811d0f7291ad311592204c
Daniel Granados <20200008@ricaldone.edu.sv>
2025-04-25 09:27:39 -0600

    verificar codigo

--- backend/src/controllers/recoveryPasswordController.js
+++ backend/src/controllers/recoveryPasswordController.js
@@ -29,11 +29,11 @@ passwordRecoveryController.requestCode = async(req, res)=>{
             res.json({message: "User not found"})
         }
 
-        const code = Math.floor(10000 + Math.random() * 90000).toString
+        const code = Math.floor(10000 + Math.random() * 90000).toString();
         const token = jsonwebtoken.sign(
             {email, code, userType, verified: false},
             config.JWT.secret,
-            {expiresIn: "20"}
+            {expiresIn: "20m"}
         )
 
         res.cookie("tokenRecoveryCode", token, {maxAge: 20*60*1000})
@@ -45,9 +45,44 @@ passwordRecoveryController.requestCode = async(req, res)=>{
             HTMLRecoveryEmail(code)
 
         )
+
+        res.json({message: "Code sent successfully"})
     } catch (error) {
         console.log("error" + error);
     }
 };
 
+passwordRecoveryController.verifyCode = async (req, res) => {
+    const { code } = req.body;
+
+    try {
+        const token = req.cookies.tokenRecoveryCode
+
+        const decoded = jsonwebtoken.verify(token, config.JWT.secret)
+
+        if(decoded.code !== code){
+            return res.json({message: "Invalid code"})
+        }
+
+        const newToken = jsonwebtoken.sign(
+            {
+                email: decoded.email, 
+                code: decoded.code,
+                userType: decoded.userType,
+                verified: true
+            },
+
+            config.JWT.secret,
+
+            {expiresIn: "20m"}
+        )
+
+        res.cookie("tokenRecoveryCode", newToken, {maxAge: 20*60*1000})
+
+        res.json({message: "Code verified successfully"})
+    } catch (error) {
+        console.log("error" + error)
+    }
+}
+ 
 export default passwordRecoveryController;
